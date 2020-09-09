64db7b2bc76dcf4b6ab5d4d92837ecdead5f1eab
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-09-09 15:33:25 +0300

    Updates

---
+++ precache-manifest.107bff5a1148bb711bc064e8d9ad9056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e90ec41786a4dd87f5afc08911284c",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "47c6388ba7a762192e3a",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.285a370c.chunk.css"
+  },
+  {
+    "revision": "8134ea8c67e1d16f441a",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6959cc3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6959cc3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c6388ba7a762192e3a",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.44b1fd25.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
